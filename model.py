27eb5e63118602592317e9f94aa91aead8416030
venkat <venkat@tcs.com>
2026-03-14 22:16:52 +0530

    updated1

--- model.py
+++ model.py
@@ -20,7 +20,9 @@ model = LinearRegression()
 model.fit(X_train, y_train)
 
 # Save the model to disk
-filename = 'model/rental_prediction_model.pkl'
+# filename = 'model/rental_prediction_model.pkl'
+
+filename = 'rental_prediction_model.pkl'
 pickle.dump(model, open(filename, 'wb'))
 
 # Evaluate the model
